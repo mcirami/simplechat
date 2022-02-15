209a4f5368a33588c01d85994225c767c1cf46ab
Matteo Cirami <mcirami@users.noreply.github.com>
2022-02-15 11:23:38 -0600

    changed register form styling, adjust login form styling

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,65 +12,67 @@
                 <!-- Validation Errors -->
                 <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
-                <div class="form_wrap {{ $addUser ? '' : 'alt' }}"
-                     style="
-                         background: url({{ asset('storage/agent-images/' . $addUser . '.jpg')}}) no-repeat;
-                         background-size: cover;
-                         background-position: top center;
-                         ">
-
-                    <form method="POST" action="{{ route('register') }}">
+                <div class="content_wrap">
+                    <div class="heading">
                         <h2>Complete the form below to be instantly connected to <span>{{$addUser}}</span> and chat live <span>FREE!</span></h2>
+                    </div>
+                    <div class="form_wrap {{ $addUser ? '' : 'alt' }}"
+                         style="
+                             background: url({{ asset('storage/agent-images/' . $addUser . '.jpg')}}) no-repeat;
+                             background-size: contain;
+                             background-position: left;">
+                        <form method="POST" action="{{ route('register') }}">
                         @csrf
 
                         <!-- UserName -->
 
                             <input id="add_chat_user" class="block mt-1 w-full" type="hidden" name="add_chat_user" value="{{$addUser}}"  />
 
-                    <!-- Name -->
-                        <div>
-                            <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus placeholder="Name"/>
-                        </div>
+                            <!-- Name -->
+                            <div>
+                                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus placeholder="Name"/>
+                            </div>
 
-                        <!-- Email Address -->
-                        <div class="mt-4">
+                            <!-- Email Address -->
+                            <div class="mt-4">
 
-                            <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required placeholder="Email"/>
-                        </div>
+                                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required placeholder="Email"/>
+                            </div>
 
-                        <!-- Password -->
-                        <div class="mt-4">
+                            <!-- Password -->
+                            <div class="mt-4">
 
-                            <x-input id="password" class="block mt-1 w-full"
-                                            type="password"
-                                            name="password"
-                                            required autocomplete="new-password"
-                                            placeholder="Password"
-                                            required
-                            />
-                        </div>
+                                <x-input id="password" class="block mt-1 w-full"
+                                         type="password"
+                                         name="password"
+                                         required autocomplete="new-password"
+                                         placeholder="Password"
+                                         required
+                                />
+                            </div>
 
-                        <!-- Confirm Password -->
-                        <div class="mt-4">
+                            <!-- Confirm Password -->
+                            <div class="mt-4">
 
-                            <x-input id="password_confirmation" class="block mt-1 w-full"
-                                            type="password"
-                                            name="password_confirmation"
-                                            placeholder="Confirm Password"
-                                     required
-                            />
-                        </div>
+                                <x-input id="password_confirmation" class="block mt-1 w-full"
+                                         type="password"
+                                         name="password_confirmation"
+                                         placeholder="Confirm Password"
+                                         required
+                                />
+                            </div>
 
-                        <div class="flex items-center justify-end mt-4">
-                            <a class="underline text-sm" href="{{ route('login') }}">
-                                {{ __('Already registered?') }}
-                            </a>
+                            <div class="flex items-center justify-end mt-4">
+                                <a class="underline text-sm" href="{{ route('login') }}">
+                                    {{ __('Already registered?') }}
+                                </a>
 
-                            <x-button class="ml-4">
-                                {{ __('Register') }}
-                            </x-button>
-                        </div>
-                    </form>
+                                <x-button class="ml-4">
+                                    {{ __('Register') }}
+                                </x-button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
 
         </x-auth-card>
