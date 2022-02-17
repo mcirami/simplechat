3efe810182fbbfbd51fe29a2c9aa3453cfb79cd0
Matteo Cirami <mcirami@users.noreply.github.com>
2022-02-17 14:13:42 -0600

    adjust login form styling

--- resources/views/vendor/Chatify/layouts/headLinks.blade.php
+++ resources/views/vendor/Chatify/layouts/headLinks.blade.php
@@ -20,7 +20,7 @@
 <link rel='stylesheet' href='https://unpkg.com/nprogress@0.2.0/nprogress.css'/>
 <link href="{{ asset('css/chatify/style.css') }}" rel="stylesheet" />
 <link href="{{ asset('css/chatify/'.$dark_mode.'.mode.css') }}" rel="stylesheet" />
-<link href="{{ asset('css/app.css?version=1.1') }}" rel="stylesheet" />
+<link href="{{ asset('css/app.css?version=1.2') }}" rel="stylesheet" />
 <link rel="icon" href="{{ asset('images/favicon.ico') }}">
 
 {{-- Messenger Color Style--}}
