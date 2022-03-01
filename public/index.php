bb78f9545c0850d0abf974e517107940dd63f964
Matteo Cirami <mcirami@users.noreply.github.com>
2022-03-01 16:26:40 -0600

    moved HTTP_CF_CONNECTING_IP to a helper file and added to composer.json

--- public/index.php
+++ public/index.php
@@ -46,10 +46,6 @@ require __DIR__.'/../vendor/autoload.php';
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-if (isset($_SERVER["HTTP_CF_CONNECTING_IP"])) {
-    $_SERVER['REMOTE_ADDR'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
-}
-
 $kernel = $app->make(Kernel::class);
 
 $response = $kernel->handle(
