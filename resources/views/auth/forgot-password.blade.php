f536b05a9122db17dfc7b621af72b86b39b34e19
Matteo Cirami <mcirami@users.noreply.github.com>
2022-03-02 11:02:35 -0600

    added div to forgot pass and reset pass forms

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -11,23 +11,25 @@
 
         <!-- Validation Errors -->
         <x-auth-validation-errors class="mb-4" :errors="$errors" />
-        <div class="form_wrap alt password_page">
-            <p class="mb-4">{{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}</p>
-            <form method="POST" action="{{ route('password.email') }}">
-                @csrf
+        <div class="content_wrap">
+            <div class="form_wrap alt password_page">
+                <p class="mb-4">{{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}</p>
+                <form method="POST" action="{{ route('password.email') }}">
+                    @csrf
 
-                <!-- Email Address -->
-                <div>
+                    <!-- Email Address -->
+                    <div>
 
-                    <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus placeholder="Email"/>
-                </div>
+                        <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus placeholder="Email"/>
+                    </div>
 
-                <div class="flex items-center justify-end mt-4">
-                    <x-button>
-                        {{ __('Email Password Reset Link') }}
-                    </x-button>
-                </div>
-            </form>
+                    <div class="flex items-center justify-end mt-4">
+                        <x-button>
+                            {{ __('Email Password Reset Link') }}
+                        </x-button>
+                    </div>
+                </form>
+            </div>
         </div>
     </x-auth-card>
 </x-guest-layout>
