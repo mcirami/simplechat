2c34ace59ed6e136ac29783d7bc2f01c1b3d95ed
Matteo Cirami <mcirami@users.noreply.github.com>
2022-03-18 07:38:04 -0500

    remove logo link and made register form fixed

--- resources/views/components/auth-card.blade.php
+++ resources/views/components/auth-card.blade.php
@@ -1,7 +1,8 @@
 <div class="min-h-screen flex justify-center items-center pt-6 sm:pt-0">
     <div class="card_content">
         <div class="logo">
-            {{ $logo }}
+            {{--{{ $logo }}--}}
+            <img src="{{asset('images/logo.png')}}" alt="" />
         </div>
         <div class="container">
             <div class="w-full mt-6 overflow-hidden white_box">
